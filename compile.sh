9a409cb6557147ba8ac1cdad6d74a76f769356e9
Peter Schmidt <petersch.home@gmail.com>
2014-01-05 17:49:39 +0100

    Fixed build and compile scripts to work with both relative and absolute paths

--- compile.sh
+++ compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+IN=`readlink -f $1`
+OUT=`readlink -f $2`
+
 OLD=`pwd`
 FILE=`readlink -f $0`
 DIR=`dirname "$FILE"`
 cd $DIR
 
-java -cp build/classes:lib/antlr-4.1-complete.jar compiler.Compiler $1 > $2
+java -cp build/classes:lib/antlr-4.1-complete.jar compiler.Compiler "$IN" > "$OUT"
 
 cd $OLD
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+IN=`readlink -f $1`
+
 OLD=`pwd`
 FILE=`readlink -f $0`
 DIR=`dirname "$FILE"`
 cd $DIR
 
-lli -load=include/helper.so $1
+lli -load=include/helper.so "$IN"
 
 cd $OLD
